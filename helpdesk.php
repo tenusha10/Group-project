8af41f1822f86c49f3edac028b74c5526c9e4425
skinner-james <56730344+skinner-james@users.noreply.github.com>
2019-10-25 13:44:59 +0100

    Update helpdesk.php

--- helpdesk.php
+++ helpdesk.php
@@ -14,7 +14,7 @@
 	
 	<body>
 		<h1>Helpdesk Operator Page</h1>
-        
+        <div>
         <form class='box'>
         <input type="button" onclick="location.href='http://google.com';" value="Log Call"/>
         <input type="button" onclick="location.href='http://google.com';" value="Active Problems"/>
@@ -22,9 +22,24 @@
         <input type="button" onclick="location.href='http://google.com';" value="Specialists"/>
         <input type="button" onclick="location.href='http://google.com';" value="Quit"/>
         </form>
+		</div>
         
         <div class="btn-group">
             
         </div>
+		<div>
+			<table>
+				<tr><th>Specalist Name</th><th>Speciality</th><th>Assigned Jobs</th></tr>
+				<?php 
+					for($i=0;$i<=10; $i=$i+1){
+						echo '<tr>
+							<td>Name'.(1+$i).'</td>
+							<td>IT</td>
+							<td>'.(($i+1)*7).'</td>
+						<tr>';
+					}
+				?>
+			</table>
+		</div>
 	</body>
 </html>
