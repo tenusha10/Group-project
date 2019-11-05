918b07137eaf7f57c8005d26a12f8dba7a8244b9
BenBalcombe19 <52495845+BenBalcombe19@users.noreply.github.com>
2019-11-05 14:51:06 +0000

    Update HelpdeskPage.php

--- HelpdeskPage.php
+++ HelpdeskPage.php
@@ -62,6 +62,8 @@
                     <h2>Other</h2>
         <!--        If the caller is  calling about something else (not sure where to submit this data if at all)-->
                     Notes: <textarea name="notes" rows="5" cols="30"></textarea><br><br><br><br><br>
+
+                    <input type="button" value="Close" onclick="hideDiv()"></input>  <br>
                     <input type="submit"><br>
                 </form>
             </div>
@@ -99,6 +101,8 @@
                     </select><br>
                     Notes: <textarea name="notes" rows="5" cols="30"></textarea><br><br><br><br><br><br>
                     <input type="checkbox" name="solved" value="solved">Solved?<br>
+
+                    <input type="button" value="Close" onclick="hideDiv()"></input>  <br>
                     <input type="submit"><br>
                 </form>
             </div>
@@ -138,6 +142,7 @@
                     </select><br>
                     Notes: <textarea name="notes" rows="5" cols="30"></textarea><br><br><br><br><br><br>
                     <input type="checkbox" name="solved" value="solved">Solved?  <br>
+                    <input type="button" value="Close" onclick="hideDiv()"></input><br>
                     <input type="submit"><br>
                 </form>
             </div>
@@ -172,6 +177,16 @@
                       Modify.style.display = "none";
                       Other.style.display = "none";
                   }
+                  }
+
+                  function hideDiv(){
+                    var New = document.getElementById("newDiv");
+                    var Modify = document.getElementById("modifyDiv");
+                    var Other = document.getElementById("otherDiv");
+
+                    New.style.display = "none";
+                    Modify.style.display = "none";
+                    Other.style.display = "none";
                   }
             </script>
 
