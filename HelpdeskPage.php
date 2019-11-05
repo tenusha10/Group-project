591c2a86f33da75cfda523f9ec78ce7d583a4939
Adam-Ling <56730350+Adam-Ling@users.noreply.github.com>
2019-11-05 13:50:15 +0000

    Location Field in tables

--- HelpdeskPage.php
+++ HelpdeskPage.php
@@ -263,7 +263,7 @@
 			<table class="specialist">
 
 
-				<thead><tr><th>Specialist Name</th><th>Speciality</th><th>Assigned Jobs</th><th>Availability</th></tr></thead>
+				<thead><tr><th>Specialist Name</th><th>Speciality</th><th>Assigned Jobs</th><th>Availability</th><th>Location</th></tr></thead>
 				<tbody>
 					<?php 
 						for($i=0;$i<=10; $i=$i+1){
@@ -273,8 +273,8 @@
 								<td>'.(($i+1)*7).'</td>
 								<td><select id="availability">
 								<option value = "available">Available</option>
-								<option value = "unavailable">Unavailable</option>
-								</td>
+								<option value = "unavailable">Unavailable</option></td>
+								<td>London</td>
 							</tr>';
 						}
 					?>
@@ -376,7 +376,7 @@
 		<a class="anchor" id="employees"></a>
 		<h2>Employees</h2>
 		<table class="employees">
-		<thead><tr><th>Name</th><th>ID number</th><th>Job Title</th><th>Department</th><th>Phone Number</th></tr></thead>
+		<thead><tr><th>Name</th><th>ID number</th><th>Job Title</th><th>Department</th><th>Phone Number</th><th>Location</th></tr></thead>
 		<tbody>
 			<?php 
 						for($i=0;$i<=10; $i=$i+1){
@@ -386,6 +386,7 @@
 								<td>Accountant</td>
 								<td>Accounting</td>
 								<td>123456</td>
+								<td>London</td>
 							</tr>';
 						}
 					?>
