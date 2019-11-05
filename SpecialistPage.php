365a2a932fd2d4e5443b6c238c2c945ed20f3cc7
BenBalcombe19 <52495845+BenBalcombe19@users.noreply.github.com>
2019-11-05 13:39:02 +0000

    Update SpecialistPage.php

--- SpecialistPage.php
+++ SpecialistPage.php
@@ -11,96 +11,100 @@
 
 </script>
     
-    <body>
-		<div class="navbar">
-			<a href="#title">Home</a>
-			<a href="#active_problems">Active Problems</a>
-			<a href="#solved_problems">Solved Problems</a>
-			<a id = "logout" href="http://team017.sci-project.lboro.ac.uk/group_project/LoginPage.html">Log Out</a>
-		</div>
+<body>
+	<div class="navbar">
+		<a href="#title">Home</a>
+		<a href="#active_problems">Active Problems</a>
+		<a href="#solved_problems">Solved Problems</a>
+		<a id = "logout" href="http://team017.sci-project.lboro.ac.uk/group_project/LoginPage.html">Log Out</a>
+	</div>
 		
-		<div class="header">
-			<a class="anchor" id="title"></a>
-			<h1>Specialist Page</h1>
-		</div>
+	<div class="header">
+		<a class="anchor" id="title"></a>
+		<h1>Specialist Page</h1>
+	</div>
     
-       <div class="container">
-			<a class="anchor" id="active_problems"></a>
-			<h2>Active Problems</h2>
-			<table class="active_problems">
-				<thead>
-					<tr>
-						<th>Number</th>
-						<th>Problem Description</th>
-						<th>Created</th>
-						<th>Logged By</th>
-						<th>Reported By</th>
-						<th>See More</th>
+    <div class="container">
+		<a class="anchor" id="active_problems"></a>
+		<h2>Active Problems</h2>
+		<table class="active_problems">
+			<thead>
+				<tr>
+					<th>Number</th>
+					<th>Problem Description</th>
+					<th>Created</th>
+					<th>Logged By</th>
+					<th>Reported By</th>
+					<th>See More</th>
 						
-						<!--
-						<th>Assigned To</th>
-						<th>Serial Number</th>
-						<th>OS</th>
-						<th>Software</th>
-						-->
-					</tr>
-				</thead>
-				<tbody>
-					<?php 
-						for($i=0;$i<=10; $i=$i+1){
-							echo '<tr>
-								<td>'.(1+$i).'</td>
-								<td>Example</td>
-								<td>01/01/2020</td>
-								<td>User1</td>
-								<td>User2</td>
-								<td><button>See More</button></td>
-							</tr>';
-						}
-					?>
-				</tbody>
-			</table>
-		</div>
+					<!--
+					<th>Assigned To</th>
+					<th>Serial Number</th>
+					<th>OS</th>
+					<th>Software</th>
+					-->
+				</tr>
+			</thead>
+			<tbody>
+				<?php 
+					for($i=0;$i<=10; $i=$i+1){
+						echo '<tr>
+							<td>'.(1+$i).'</td>
+							<td>Example</td>
+							<td>01/01/2020</td>
+							<td>User1</td>
+							<td>User2</td>
+							<td><button>See More</button></td>
+						</tr>';
+					}
+				?>
+			</tbody>
+		</table>
+	</div>
 		
-		<div id="modifyDiv">
-                <form action="modify_problem.php" method="post">
-        <!--        If the operator needs to modify an existing problem (caller specifies their problem ID) then that
-                    corresponding problem data fills the input things and the operator can change them and resubmit it to
-                    the DB-->
-                    <br><br>
-                    <h2>Modify Problem: </h2><br>
-                    Problem Num: <input type="text" name="num"><br>
-                    Description: <input type="text" name="caller"><br>
-                    Problem Type: <select name="problemType">
-                    <option value="">Select...</option>
-                    <optgroup label="Hardware">
-                        <option value="printer">Printer</option>
-                        <option value="printerJam">&nbsp;--Jammed</option>
-                        <option value="printerRefill">&nbsp;--Needs more paper / ink</option>
-                        <option value="phone">Phone</option>
-                    </optgroup>
-                    <optgroup label="Software">
-                        <option value="renewal">Need renewal</option>
-                        <option value="update">Update</option>
-                    </optgroup>
-                    <optgroup label="Network">
-                        <option value="wifi">Wifi Down</option>
-                    </optgroup>
-                    </select><br>
-                    Hardware Serial: <input type="text" name="serial"><br>
-                    OS: <input type="text" name="OS"><br>
-                    Software: <input type="text" name="software"><br>
-                    Specialist: <select name="specialist">
-                    <option value="">Select...</option>
-                    <option value="bert">Bert</option>
-                    <option value="clara">Clara</option>
-                    </select><br>
-                    Notes: <textarea name="notes" rows="5" cols="30"></textarea><br><br><br><br><br><br>
-                    <input type="checkbox" name="solved" value="solved">Solved?  <br>
-                    <input type="submit"><br>
-                </form>
-            </div>
 
+	<div class="log_call_container">
+		<a class="anchor" id="log_call"></a>
+		<h2 style="text-align: center;">Log a Call</h2>
+		<div id="modifyDiv">
+				<form action="modify_problem.php" method="post">
+		<!--        If the operator needs to modify an existing problem (caller specifies their problem ID) then that
+					corresponding problem data fills the input things and the operator can change them and resubmit it to
+					the DB-->
+					<br><br>
+					<h2>Modify Problem: </h2><br>
+					Problem Num: <input type="text" name="num"><br>
+					Description: <input type="text" name="caller"><br>
+					Problem Type: <select name="problemType">
+					<option value="">Select...</option>
+					<optgroup label="Hardware">
+						<option value="printer">Printer</option>
+						<option value="printerJam">&nbsp;--Jammed</option>
+						<option value="printerRefill">&nbsp;--Needs more paper / ink</option>
+						<option value="phone">Phone</option>
+					</optgroup>
+					<optgroup label="Software">
+						<option value="renewal">Need renewal</option>
+						<option value="update">Update</option>
+					</optgroup>
+					<optgroup label="Network">
+						<option value="wifi">Wifi Down</option>
+					</optgroup>
+					</select><br>
+					Hardware Serial: <input type="text" name="serial"><br>
+					OS: <input type="text" name="OS"><br>
+					Software: <input type="text" name="software"><br>
+					Specialist: <select name="specialist">
+					<option value="">Select...</option>
+					<option value="bert">Bert</option>
+					<option value="clara">Clara</option>
+					</select><br>
+					Notes: <textarea name="notes" rows="5" cols="30"></textarea><br><br><br><br><br><br>
+					<input type="checkbox" name="solved" value="solved">Solved?  <br>
+					<input type="submit"><br>
+				</form>
+			</div>
+		</div>
 
 
 
