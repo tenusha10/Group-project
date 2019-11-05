d9a986e0323042fe9c192aa98823a3e491745d44
Adam-Ling <56730350+Adam-Ling@users.noreply.github.com>
2019-11-05 13:20:34 +0000

    Employees Link & Navbar Text shrunk

--- HelpdeskPage.php
+++ HelpdeskPage.php
@@ -15,6 +15,7 @@
 			<a href="#active_problems">Active Problems</a>
 			<a href="#solved_problems">Solved Problems</a>
 			<a href="#specialist">Specialists</a>
+			<a href="#employees">Employees</a>
 			<a id = "logout" href="http://team017.sci-project.lboro.ac.uk/group_project/LoginPage.html">Log Out</a>
 		</div>
 		<div class="header">
