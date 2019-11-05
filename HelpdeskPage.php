432ae09e54cfba270a792c30433dacd31a4b9f5c
Adam-Ling <56730350+Adam-Ling@users.noreply.github.com>
2019-11-05 13:41:32 +0000

    fix

--- HelpdeskPage.php
+++ HelpdeskPage.php
@@ -16,7 +16,6 @@
 			<a href="#solved_problems">Solved Problems</a>
 			<a href="#specialist">Specialists</a>
 			<a href="#employees">Employees</a>
-            <a href="#specialist">Specialists</a>
             <a href="#addEmployee">Add User</a>
 			<a id = "logout" href="http://team017.sci-project.lboro.ac.uk/group_project/LoginPage.html">Log Out</a>
 		</div>
@@ -391,25 +390,5 @@
 						}
 					?>
 		</tbody>
-		</div>
-		<div class="container">
-		<a class="anchor" id="employees"></a>
-		<h2>Employees</h2>
-		<table class="employees">
-		<thead><tr><th>Name</th><th>ID number</th><th>Job Title</th><th>Department</th><th>Phone Number</th></tr></thead>
-		<tbody>
-			<?php 
-						for($i=0;$i<=10; $i=$i+1){
-							echo '<tr>
-								<td>Name'.(1+$i).'</td>
-								<td>123</td>
-								<td>Accountant</td>
-								<td>Accounting</td>
-								<td>123456</td>
-							</tr>';
-						}
-					?>
-		</tbody>
-		</div>
 	</body>
 </html>
