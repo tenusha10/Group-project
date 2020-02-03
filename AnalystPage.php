2927c8de4c05926ddc4fadae841fde99c6ca85c3
tenusha10 <55375054+tenusha10@users.noreply.github.com>
2020-02-03 14:51:41 +0000

    Create AnalystPage.php

---
+++ AnalystPage.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+
+		<title>Analyst</title>
+		
+        <link href="Users.css" rel="stylesheet">
+		
+	</head>
+    <div class="header">
+			<a class="anchor" id="title"></a>
+			<h1>Analyst Page</h1>
+		</div>
+        <body>
+
+		<div class="navbar">
+			<a href="#title">Home</a>
+			<a href="#specialist">Specialist</a>
+			<a href="#equipment_software">Equipment & software</a>
+			<a href="#training">Training</a>
+			<a id = "logout" href="http://team017.sci-project.lboro.ac.uk/group_project/LoginPage.html">Log Out</a>
+		</div>
+        <div class="container">
+			<a class="anchor" id="active_problems"></a>
+			<h2>Active Problems</h2>
+			<table class="active_problems">
+				<thead>
+        </body>
