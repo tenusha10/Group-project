ecf1927c9c6536fc2f4832deff87e6339d8d4f90
BenBalcombe19 <52495845+BenBalcombe19@users.noreply.github.com>
2020-02-04 11:18:23 +0000

    Update AnalystPage.php

--- AnalystPage.php
+++ AnalystPage.php
@@ -19,10 +19,48 @@
 			<a href="#equipment_software">Equipment & software</a>
 			<a href="#training">Training</a>
 			<a id = "logout" href="http://team017.sci-project.lboro.ac.uk/group_project/LoginPage.php">Log Out</a>
-		</div>
-        <div class="container">
-			<a class="anchor" id="active_problems"></a>
-			<h2>Active Problems</h2>
-			<table class="active_problems">
-				<thead>
+	</div>
+    
+	 <div class="container">
+		<a class="anchor" id="specialistStats"></a>
+		<h2>Specialist Stats</h2>
+		<table class="specialist_stats">
+			<thead>
+				<tr>
+					<th>Number</th>
+					<th>Problem Description</th>
+					<th>Created</th>
+					<th>Logged By</th>
+					<th>Reported By</th>
+					<th>See More</th>
+						
+					<!--
+					<th>Assigned To</th>
+					<th>Serial Number</th>
+					<th>OS</th>
+					<th>Software</th>
+					-->
+				</tr>
+			</thead>
+			<tbody>
+				<?php 
+					for($i=0;$i<10; $i=$i+1){
+						echo '<tr>
+							<td>'.(1+$i).'</td>
+							<td>Example</td>
+							<td>01/01/2020</td>
+							<td>User1</td>
+							<td>User2</td>
+							<td><button>See More</button></td>
+						</tr>';
+					}
+				?>
+			</tbody>
+		</table>
+	</div>
+
+
+
+		
+		
         </body>
